c383a254dbf64e9889da84167350446a6c23a867
xiaolong <shuangpk01@qq.com>
2019-06-21 10:19:09 +0800

    init

---
+++ src/main/java/com/liyan/LiYanBlogsApplication.java
@@ -0,0 +1,13 @@
+package com.liyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiYanBlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiYanBlogsApplication.class, args);
+	}
+
+}
